2787324e39f97e83bf9b652201ec424f49afe2f3
KilianFW <k.flament@nouvellesimages.xyz>
2026-04-09 09:36:04 +0200

    Add Falling Platform

---
+++ Assets/Scripts/Falling Platform.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Data.SqlTypes;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float timer;
+    public float falltimer;
+    public float respawntimer;
+    public bool platformVisible = true;
+    
+    [SerializeField] LayerMask groundLayer;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        timer = falltimer;
+        
+        /*/*if (timer < 0f)
+        {
+            Debug.Log("Falling Platform");
+            gameObject.SetActive(false);
+        }#1#
+    }
+    if (gameObject.activeSelf == false)
+    {
+        timer = respawntimer;
+        /*if (timer < 0f)
+        {
+            gameObject.SetActive(true);
+        }#1#*/
+
+    }
+
+    /*public void PlatformToggle()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+            timer = respawntimer;
+        }
+        else gameObject.SetActive(true);
+    }*/
+
+    public void PlatformToggle()
+    {
+        if (platformVisible)
+        {
+            gameObject.SetActive(false);
+            platformVisible = false;
+            timer = respawntimer;
+        }
+        else gameObject.SetActive(true);
+    }
+    
+
+    private void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                if (platformVisible == true)
+                {
+                    PlatformToggle();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -3,39 +3,15 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public Camera cam;
     public Transform followTarget;
-    
-    // Starting position for the parallax game object
-    private Vector2 startingPosition;
-    
-    // Start Z value of the parallax game object
-    private float startingZ;
-    
-    // Distance that the camera has moved from the starting position of the parallax object
-    private Vector2 camMoveSinceStart => (Vector2)cam.transform.position - startingPosition;
-    
-    //
-    private float zDistanceFromTarget => transform.position.z - followTarget.transform.position.z;
+    public Vector3 offSet;
+    public Vector3 moveMultiplier;
+  
 
-    // If object is in front of target, use near clip plane. If behind target, use FarClipPlane
-    private float clippingPlane => (cam.transform.position.z + (zDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane));
-    
-    // The further the object from the player, the faster the ParallaxEffect object will move. Drag it's Z value closer to the target to make it move slower.
-    private float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
-        
-    private void Start()
-    {
-        startingPosition = cam.transform.position;
-        startingZ = transform.position.z;
-    }
 
     private void Update()
     {
-        // When the target moves, move the parallax object the same distance times a multiplier
-        Vector2 newPosition = startingPosition + camMoveSinceStart * parallaxFactor;
-        
-        // The X/Y position changes based on target travel speed times the parallax factor, but Z stays consistent
-        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);
+
+        transform.position = new Vector3(followTarget.position.x * moveMultiplier.x + offSet.x, followTarget.position.y * moveMultiplier.y + offSet.y, followTarget.position.z * moveMultiplier.z + offSet.z);
     }
 }
